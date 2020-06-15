a73c51cb9271d4ae736d8bc715274b4c4c94ebc3
Toukiloglou <toukiloglou@gmail.com>
2020-06-15 23:47:39 +0300

    Action and Lamda expressions

---
+++ DelegatesActionsEvents/Assets/Scripts/Callback.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Callback : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(MyRoutine( () => {
+            Debug.Log("The routine has finished");
+        }));
+    }
+
+    public IEnumerator MyRoutine(Action onComplete = null) {
+
+        yield return new WaitForSeconds(5f);
+
+        if(onComplete != null) {
+            onComplete();
+        }
+
+    }
+}
